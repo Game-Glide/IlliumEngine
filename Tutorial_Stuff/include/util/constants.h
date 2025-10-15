b43bd482309fd0fa0d219e38a11056677fe8c491
Game Glide <39207327+Game-Glide@users.noreply.github.com>
2025-10-15 15:33:37 +0530

    added some todos

--- Tutorial_Stuff/include/util/constants.h
+++ Tutorial_Stuff/include/util/constants.h
@@ -1,6 +1,7 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+// TODO: Make these not OS dependant
 #define PROJECT_ROOT "/home/game_glide/Projects/opengl-experiments/Tutorial_Stuff"
 #define V_SHADER_DIR "/home/game_glide/Projects/opengl-experiments/Tutorial_Stuff/resources/shaders/vertex/"
 #define F_SHADER_DIR "/home/game_glide/Projects/opengl-experiments/Tutorial_Stuff/resources/shaders/fragment/"
